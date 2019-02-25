3c94aead822a41b65314042ce1babab7f5d33943
Telly Martel-Audion <telly.martel-audion@isen.yncrea.fr>
2019-02-25 15:44:12 +0100

    Authentication done (Sign up and Sign in)

--- app/src/main/java/fr/isen/cata/werewolfapp/SignInActivity.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/SignInActivity.kt
@@ -1,12 +1,47 @@
 package fr.isen.cata.werewolfapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        var email:String
+        var password:String
+
+        auth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener{
+
+            email = emailContainerIn.text.toString()
+            password = passwordContainerIn.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, start HomeActivity
+                        Log.d("TAG", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
