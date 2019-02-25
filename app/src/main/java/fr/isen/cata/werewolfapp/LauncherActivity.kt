2ee4e01e3d59e0ea6f3c9a912c09b218fe507ffb
Telly Martel-Audion <telly.martel-audion@isen.yncrea.fr>
2019-02-25 21:49:48 +0100

    Design Launcher, LauncherText animation

--- app/src/main/java/fr/isen/cata/werewolfapp/LauncherActivity.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/LauncherActivity.kt
@@ -1,12 +1,15 @@
 package fr.isen.cata.werewolfapp
 
+import android.animation.ValueAnimator
+import android.animation.ValueAnimator.INFINITE
+import android.animation.ValueAnimator.REVERSE
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.BounceInterpolator
 import kotlinx.android.synthetic.main.activity_launcher.*
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LauncherActivity : Activity() {
 
@@ -14,11 +17,29 @@ class LauncherActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
 
+        animateLaunchText()
+
         launcherLayout.setOnClickListener { v: View ->
 
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun animateLaunchText() {
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            LaunchText.alpha = value
+        }
+
+        valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+        valueAnimator.duration = 1700
+        valueAnimator.repeatCount = INFINITE
+        valueAnimator.repeatMode = REVERSE
+
+        valueAnimator.start()
     }
 
 }
