f3a97f2ea6fd0366869ad933a530cfa8d1cca7ac
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-25 15:38:34 +0100

    ajout image

--- app/src/main/java/fr/isen/cata/werewolfapp/HomeActivity.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/HomeActivity.kt
@@ -1,12 +1,22 @@
 package fr.isen.cata.werewolfapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        createButton.setOnClickListener {
+            startActivity(Intent(this, CreateActivity::class.java))
+        }
+
+        joinButton.setOnClickListener {
+            startActivity(Intent(this, JoinActivity::class.java))
+        }
     }
 }
