a363b3c3499b628b7e08d923578dd9b737ea5b3f
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-25 19:28:18 +0100

    create activity changement

--- app/src/main/java/fr/isen/cata/werewolfapp/CreateActivity.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/CreateActivity.kt
@@ -1,15 +1,54 @@
 package fr.isen.cata.werewolfapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_create.*
 
 class CreateActivity : AppCompatActivity() {
+
+    private var nbPlayer = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        var mDatabase: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+        nbPlayerView.text = nbPlayer.toString()
+
+        returnButton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        addButton.setOnClickListener {
+            if(nbPlayer < 18) {
+                nbPlayer += 1
+            } else {
+                Toast.makeText(this, "maximum 18 joueurs!", Toast.LENGTH_LONG).show()
+            }
+            nbPlayerView.text = nbPlayer.toString()
+        }
+
+        removeButton.setOnClickListener {
+            if(nbPlayer > 4){
+                nbPlayer -= 1
+            } else {
+                Toast.makeText(this, "minimum 4 joueurs!", Toast.LENGTH_LONG).show()
+            }
+            nbPlayerView.text = nbPlayer.toString()
+        }
+
+        createButton.setOnClickListener {
+            onCreate()
+        }
     }
 
-
-
+    private fun onCreate() {
+        var partyName: String = partyNameView.text.toString()
+        val idParty = 6
+    }
 }
