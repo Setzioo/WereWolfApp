a8c028db50d074a607b724b4b38f634582dac362
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-26 10:27:44 +0100

    create user sign Up

--- app/src/main/java/fr/isen/cata/werewolfapp/PlayerModel.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/PlayerModel.kt
@@ -2,12 +2,12 @@ package fr.isen.cata.werewolfapp
 
 import android.graphics.Bitmap
 
-class PlayerModel(_id: Int=0, _pseudo: String="", _avatar: Bitmap?=null, _role: RoleModel?=null, _state: Boolean=true, _charmed: Boolean=false, _connected: Boolean=false) {
-    var id: Int = _id
+class PlayerModel(_id: String="0", _pseudo: String=""/*, _avatar: Bitmap?=null, _role: RoleModel?=null, _state: Boolean=true, _charmed: Boolean=false, _connected: Boolean=false*/) {
+    var id: String = _id
     var pseudo: String = _pseudo
-    var avatar: Bitmap? = _avatar
-    var role: RoleModel? = _role
-    var state: Boolean = _state
-    var charmed: Boolean = _charmed
-    var connected: Boolean = _connected
+    var avatar: Bitmap? = null
+    var role: RoleModel? = null
+    var state: Boolean = true
+    var charmed: Boolean = false
+    var connected: Boolean = false
 }
