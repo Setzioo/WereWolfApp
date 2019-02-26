734fc4f5a3daf329f4661738c74ce51e4d5e72e5
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-26 09:45:56 +0100

    class model updated

--- app/src/main/java/fr/isen/cata/werewolfapp/PlayerModel.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/PlayerModel.kt
@@ -1,11 +1,13 @@
 package fr.isen.cata.werewolfapp
 
-class PlayerModel {
-    var id: Int = 0
-    var pseudo: String = ""
-    var avatar: String = ""
-    var role: RoleModel? = null
-    var state: Boolean = true
-    var charmed: Boolean = false
-    var connected: Boolean = false
+import android.graphics.Bitmap
+
+class PlayerModel(_id: Int=0, _pseudo: String="", _avatar: Bitmap?=null, _role: RoleModel?=null, _state: Boolean=true, _charmed: Boolean=false, _connected: Boolean=false) {
+    var id: Int = _id
+    var pseudo: String = _pseudo
+    var avatar: Bitmap? = _avatar
+    var role: RoleModel? = _role
+    var state: Boolean = _state
+    var charmed: Boolean = _charmed
+    var connected: Boolean = _connected
 }
