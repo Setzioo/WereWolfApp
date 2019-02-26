d7193d5d29acde77c37d35ff27541ca21c53fc36
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-26 15:10:27 +0100

    RecyclerView lobbies

--- app/src/main/java/fr/isen/cata/werewolfapp/JoinActivity.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/JoinActivity.kt
@@ -2,11 +2,45 @@ package fr.isen.cata.werewolfapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import android.widget.LinearLayout
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_join.*
 
 class JoinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        val lobbies: ArrayList<LobbyModel?> = ArrayList()
+
+        lobbyView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+        val adapter = LobbyAdapter(lobbies)
+        lobbyView.adapter = adapter
+
+        val mLobbyReference = FirebaseDatabase.getInstance().getReference("Lobby")
+
+        mLobbyReference.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //val user: MutableList<LobbyModel?> = arrayListOf()
+                if (dataSnapshot.exists()) {
+                    for(i in dataSnapshot.children){
+                        //user.add(i.getValue(LobbyModel::class.java))
+                        lobbies.add(i.getValue(LobbyModel::class.java))
+                        (lobbyView.adapter as LobbyAdapter).notifyDataSetChanged()
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("TAG", "loadPost:onCancelled", databaseError.toException())
+            }
+        })
+
     }
 }
