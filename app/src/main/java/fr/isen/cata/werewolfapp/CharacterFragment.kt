d5ff3bcab5547d40fbab0073e820274744d52203
Arno <arno.lopasso@isen.yncrea.fr>
2019-03-01 18:03:33 +0100

    Fragments

---
+++ app/src/main/java/fr/isen/cata/werewolfapp/CharacterFragment.kt
@@ -0,0 +1,90 @@
+package fr.isen.cata.werewolfapp
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.os.Vibrator
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat.getSystemService
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.widget.TextView
+import kotlinx.android.synthetic.main.layout_character.*
+
+
+class CharacterFragment : Fragment() {
+private val context=this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.layout_character, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        animateCards()
+
+
+    }
+
+
+
+
+
+    private fun animateCards() {
+        val valueAnimator = ValueAnimator.ofFloat(0f, 720f)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+
+            Cards.rotation =value
+
+        }
+        val valueAnimator1 = ValueAnimator.ofFloat(0f, 1f)
+
+        valueAnimator1.addUpdateListener {
+            val value = it.animatedValue as Float
+            Cards.alpha=value
+
+
+        }
+
+
+        valueAnimator.interpolator = AccelerateInterpolator()
+        valueAnimator.duration = 2000
+        valueAnimator1.interpolator = AccelerateInterpolator()
+        valueAnimator1.duration = 2000
+
+
+
+        valueAnimator.start()
+        valueAnimator1.start()
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+
+    companion object {
+        fun newInstance() = CharacterFragment()
+    }
+}
