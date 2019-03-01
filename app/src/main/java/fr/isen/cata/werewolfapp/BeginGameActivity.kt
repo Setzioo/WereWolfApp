e5fd39cf8f184e08f4e56bdc73f0d5145caffaf9
Camille Arbousset <camille.arbousset@isen.yncrea.fr>
2019-03-01 08:39:08 +0100

    Déplacement attribution role Begin -> Lobby

--- app/src/main/java/fr/isen/cata/werewolfapp/BeginGameActivity.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/BeginGameActivity.kt
@@ -2,21 +2,10 @@ package fr.isen.cata.werewolfapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-
-
 
 
 
 class BeginGameActivity : AppCompatActivity() {
-
-    private lateinit var mDatabase: DatabaseReference
-    private lateinit var mLobbyReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
