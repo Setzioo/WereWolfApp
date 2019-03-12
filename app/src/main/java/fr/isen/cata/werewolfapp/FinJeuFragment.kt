93d80a492a272fa9431fba6ed2975388fc12428e
Arno <arno.lopasso@isen.yncrea.fr>
2019-03-12 16:06:51 +0100

    sound et debut fragment jeu

--- app/src/main/java/fr/isen/cata/werewolfapp/FinJeuFragment.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/FinJeuFragment.kt
@@ -1,19 +1,23 @@
 package fr.isen.cata.werewolfapp
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.view.animation.AccelerateInterpolator
+import kotlinx.android.synthetic.main.layout_character.*
 
 
 class FinJeuFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        animateCards()
 
     }
 
@@ -25,6 +29,42 @@ class FinJeuFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_fin_jeu, container, false)
     }
 
+    private fun animateCards() {
+        val valueAnimator = ValueAnimator.ofFloat(0f, 720f)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+
+            Cards.rotation =value
+
+        }
+        val valueAnimator1 = ValueAnimator.ofFloat(0f, 1f)
+
+        valueAnimator1.addUpdateListener {
+            val value = it.animatedValue as Float
+            Cards.alpha=value
+
+
+        }
+
+
+        valueAnimator.interpolator = AccelerateInterpolator()
+        valueAnimator.duration = 2000
+        valueAnimator1.interpolator = AccelerateInterpolator()
+        valueAnimator1.duration = 2000
+
+
+
+        valueAnimator.start()
+        valueAnimator1.start()
+
+        Handler().postDelayed({
+            val manager = MyFragmentManager()
+            manager.DebutNuitFragment(context!!)
+        },7000)
+
+
+    }
 
     /**
      * This interface must be implemented by activities that contain this
