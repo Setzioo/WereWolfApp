0ff9026a4b7bf8904d05d7815a3ee6cfc8066366
Arno <arno.lopasso@isen.yncrea.fr>
2019-03-15 19:17:25 +0100

    chrono presque fini manque plus que relier

--- app/src/main/java/fr/isen/cata/werewolfapp/VideoFragment.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/VideoFragment.kt
@@ -1,5 +1,6 @@
 package fr.isen.cata.werewolfapp
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.VideoView
 import kotlinx.android.synthetic.main.activity_launcher.*
 import kotlinx.android.synthetic.main.fragment_video.*
@@ -24,6 +26,7 @@ class VideoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_video, container, false)
+        animateLaunchText()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,16 +40,31 @@ class VideoFragment : Fragment() {
             val manager = MyFragmentManager()
             manager.CharacterFragment(context!!)
         }*/
-        object : CountDownTimer(30000, 1000) {
+        object : CountDownTimer(5000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
-                compteur.setText("" + millisUntilFinished / 1000)
+                compteur.setText("" + (millisUntilFinished / 1000+1))
             }
 
             override fun onFinish() {
-                compteur.setText("done!")
+                compteur.setText("GO")
             }
         }.start()
+        animateLaunchText()
+    }
+    private fun animateLaunchText() {
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            compteur.alpha = value
+        }
+
+        valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+        valueAnimator.duration = 548
+        valueAnimator.repeatCount = ValueAnimator.INFINITE
+        valueAnimator.repeatMode = ValueAnimator.REVERSE
+        valueAnimator.start()
     }
 
 
