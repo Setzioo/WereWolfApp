6b0d4ef2bee28b7fdd04c7c75efb10bb05a93df0
Telly Martel-Audion <telly.martel-audion@isen.yncrea.fr>
2019-03-18 17:12:17 +0100

    FragmentLoup FINIIIIII

--- app/src/main/java/fr/isen/cata/werewolfapp/LoupFragment.kt
+++ app/src/main/java/fr/isen/cata/werewolfapp/LoupFragment.kt
@@ -1,28 +1,25 @@
 package fr.isen.cata.werewolfapp
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridLayout
-import android.widget.LinearLayout
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.activity_lobby.*
 import kotlinx.android.synthetic.main.fragment_loup.*
-
-//TODO : Vote parmi les vivants
 
 
 class LoupFragment : Fragment() {
 
     private lateinit var mDatabase: DatabaseReference
-    private lateinit var adapter: LoupAdapter
+    private lateinit var loupAdapter: LoupAdapter
+    private lateinit var wolfieAdapter: WolfieAdapter
 
     private lateinit var auth: FirebaseAuth
 
@@ -30,6 +27,7 @@ class LoupFragment : Fragment() {
     var gameName : String =""
     var game : PartyModel? = null
     var listId : MutableList<String>? = arrayListOf()
+    private val compteurMax: Long = 5
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,21 +37,114 @@ class LoupFragment : Fragment() {
         Toast.makeText(context, "Loups", Toast.LENGTH_LONG).show()
 
         loupRecyclerView.layoutManager = GridLayoutManager(context!!,2)
+        wolfies.layoutManager = GridLayoutManager(context!!,1, GridLayoutManager.HORIZONTAL, false)
 
         val players: ArrayList<PlayerModel?> = ArrayList()
+        val wolfs : ArrayList<PlayerModel?> = ArrayList()
 
-        adapter = LoupAdapter(players)
-        loupRecyclerView.adapter = adapter
+        loupAdapter = LoupAdapter(players)
+        wolfieAdapter = WolfieAdapter(wolfs)
+
+        loupRecyclerView.adapter = loupAdapter
+        wolfies.adapter = wolfieAdapter
 
         initVotes()
 
         getVillagers(players)
+
+        getWerewolves(wolfs)
+
+        object : CountDownTimer(compteurMax*1000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = "" + (millisUntilFinished / 1000+1)
+                wolfieTiming.text = timeLeft
+            }
+
+            override fun onFinish() {
+                wolfieTiming.text = "0"
+                Handler().postDelayed({
+                    endOfWolfiesTurn()
+                },1500)
+            }
+        }.start()
 
 
 
 
 
     }
+
+    private fun endOfWolfiesTurn() {
+
+        val mUserReference = FirebaseDatabase.getInstance().getReference("")
+        auth = FirebaseAuth.getInstance()
+        val id: String = auth.currentUser!!.uid
+
+        var idToKill = ""
+        var equality = false
+        var nbVotesMax = 0
+
+        mUserReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val users: MutableList<PlayerModel?> = arrayListOf()
+                if (dataSnapshot.exists()) {
+                    for (i in dataSnapshot.child("Users").children) {
+                        users.add(i.getValue(PlayerModel::class.java))
+                    }
+                    for (i in users) {
+                        if (i?.id == id) {
+                            currentPlayer = i
+                            gameName = currentPlayer!!.currentGame!!
+                        }
+                    }
+                }
+                if (dataSnapshot.exists()) {
+                    game = dataSnapshot.child("Party").child(gameName).getValue(PartyModel::class.java)
+                    if(game!=null){
+                        if(game!!.listPlayer != null){
+                            listId = game!!.listPlayer
+                        }
+                    }
+                }
+                if (listId != null) {
+                    for(i in listId!!){
+                        for(u in dataSnapshot.child("Users").children){
+                            val user = u.getValue(PlayerModel::class.java)
+                            if(i == user!!.id){
+                                if(user.nbVotesLoup > nbVotesMax)
+                                {
+                                    nbVotesMax = user.nbVotesLoup
+                                    equality = false
+                                    idToKill = user.id
+                                }
+                                else if (user.nbVotesLoup == nbVotesMax)
+                                {
+                                    equality = true
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if(!equality)
+                {
+                    mUserReference.child(idToKill).child("state").setValue(false)
+                }
+
+                mDatabase.child("Party").child(gameName).child("FinishFlags").child("LoupFlag").setValue(true)
+
+                val manager = MyFragmentManager()
+                manager.NightFragment(context!!)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("TAG", "loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
+
 
     private fun getVillagers(players: ArrayList<PlayerModel?>) {
 
@@ -90,7 +181,56 @@ class LoupFragment : Fragment() {
                             if(i == user!!.id){
                                 if(user.id != currentPlayer!!.id && user.state && user.role!="Loup-Garou") {
                                     players.add(user)
-                                    adapter.notifyDataSetChanged()
+                                    loupAdapter.notifyDataSetChanged()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("TAG", "loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
+
+    private fun getWerewolves(players: ArrayList<PlayerModel?>) {
+
+        val mUserReference = FirebaseDatabase.getInstance().getReference("")
+        auth = FirebaseAuth.getInstance()
+        val id: String = auth.currentUser!!.uid
+
+        mUserReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val users: MutableList<PlayerModel?> = arrayListOf()
+                if (dataSnapshot.exists()) {
+                    for (i in dataSnapshot.child("Users").children) {
+                        users.add(i.getValue(PlayerModel::class.java))
+                    }
+                    for (i in users) {
+                        if (i?.id == id) {
+                            currentPlayer = i
+                            gameName = currentPlayer!!.currentGame!!
+                        }
+                    }
+                }
+                if (dataSnapshot.exists()) {
+                    game = dataSnapshot.child("Party").child(gameName).getValue(PartyModel::class.java)
+                    if(game!=null){
+                        if(game!!.listPlayer != null){
+                            listId = game!!.listPlayer
+                        }
+                    }
+                }
+                if (listId != null) {
+                    for(i in listId!!){
+                        for(u in dataSnapshot.child("Users").children){
+                            val user = u.getValue(PlayerModel::class.java)
+                            if(i == user!!.id){
+                                if(user.state && user.role=="Loup-Garou") {
+                                    players.add(user)
+                                    wolfieAdapter.notifyDataSetChanged()
                                 }
                             }
                         }
---
+++ app/src/main/java/fr/isen/cata/werewolfapp/WolfieAdapter.kt
@@ -0,0 +1,93 @@
+package fr.isen.cata.werewolfapp
+
+import android.graphics.Color
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+
+class WolfieAdapter(private val players: ArrayList<PlayerModel?>): RecyclerView.Adapter<WolfieAdapter.ViewHolder>() {
+
+    val mDatabase = FirebaseDatabase.getInstance().reference
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        auth = FirebaseAuth.getInstance()
+        //holder.pseudo.text = players[position]!!
+        getPlayerAvatar(holder, players[position]!!)
+        holder.pseudo.text = players[position]!!.pseudo
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.loup_horizontal_row, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return players.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var avatar: ImageView = itemView.findViewById(R.id.wolfieFace)
+        var pseudo: TextView = itemView.findViewById(R.id.wolfieName)
+    }
+
+    private fun getPlayerAvatar(holder: ViewHolder, player: PlayerModel) {
+        val storageReference = FirebaseStorage.getInstance().reference.child(player.id + "/avatar")
+
+        storageReference.downloadUrl.addOnSuccessListener {
+            // Got the download URL for 'users/me/profile.png'
+            Picasso.get()
+                .load(it)
+                .into(holder.avatar)
+        }.addOnFailureListener {
+            // Handle any errors
+        }
+    }
+
+    /*
+    private fun idIntoPlayerModel(idPlayer: String, holder: LoupAdapter.ViewHolder) {
+
+        val mUserReference = FirebaseDatabase.getInstance().getReference("Users")
+
+        mUserReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user: MutableList<PlayerModel?> = arrayListOf()
+                if (dataSnapshot.exists()) {
+                    user.clear()
+                    for (i in dataSnapshot.children) {
+                        user.add(i.getValue(PlayerModel::class.java))
+                    }
+                    for (i in user) {
+                        if (i?.id ==idPlayer) {
+                            playerInModel = i
+                            getPlayerAvatar(holder, playerInModel!!)
+                            holder.pseudo.text = playerInModel!!.pseudo
+                        }
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("TAG", "loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
+    */
+
+}
+
+
+
